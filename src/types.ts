4ef5167f75c1f034b54990ef9177d6a1d90e189c
aiden-jung <jimmy53120488@gmail.com>
2020-09-13 16:41:44 +0900

    FIX: 현재 사용하지 않는 MyContext 타입 제거

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,3 @@ export type DecodedAccessToken = {
 export type MyState = {
   user?: DecodedAccessToken;
 };
-
-export type MyContext = {
-  error?: (status: number, message: string) => Promise<void>;
-};
